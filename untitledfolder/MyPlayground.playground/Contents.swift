3ee5c3732c9307aa70b39088e497019d89db6abb
nguyen8k@uregin.ca <nguyen8k@uregina.ca>
2021-03-28 00:06:05 -0600

    first commit

---
+++ untitledfolder/MyPlayground.playground/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+//var, let, type
+var a:Int = 2
+var b : Int  // thieu gia tri b
+let c : Int = 5 // cannot change the value because let
+
+var t : String = "Hi everybody !"
+a = 4 + 1
+print (a)
+
+//if ; == ; != co khac hay khong
+if a>5{
+    print("Today is monday")
+}
+else {
+    print ( "Today is sunday")
+}
+if a == 5 {
+    print(t)
+    }
+else {
+    print (c)
+}
+
+// not = 6
+if a != 6 {
+    print(" a < 6 ")
+}
+else{
+    print(t)
+}
+// // function : if, for, while - repeated - vong lap
+print(1)
+print(2)
+print(3)
+
+//print from 1 to 8 -for loop
+for i in 1...8{
+    
+    print (" Dong so :" + String(i)) // string + string is ok
+    
+    //or just  print (i)
+}
+
+var m : Int = 1
+while m < 5{
+    print (m)
+    
+    m=m+1
+}
+
+
+
+
+//array
+
